7ec35122c7b2d2e71d34751ba96c5b54daceed50
fainnerramirez <fainnerramirez@gmail.com>
2026-05-21 11:39:03 -0500

    feat: add orbit component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ChakraProvider } from "@chakra-ui/react"
+import { system } from "@chakra-ui/react/preset"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import OrbitingCirclesDemo from "./components/orbit.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider value={system} >
+      <OrbitingCirclesDemo />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
